c706153073d94ef579eb6193f1a4241bfa7e9f67
andrewcdominiakyt-prog <andrew.c.dominiakyt@gmail.com>
2025-08-26 23:17:51 +0000

    keep working

--- TicTacTo.py
+++ TicTacTo.py
@@ -2,6 +2,7 @@ player1 = "X"
 player2 = "O"
 
 
+
 one_one = 0
 one_two = 0
 one_three = 0
@@ -24,53 +25,133 @@ def print_board():
     for row in board:
         print(' '.join([str(cell) for cell in row]))
 
-print_board()
 
 def player1turn():  
     if player1_turn == True: 
         print("player 1 turn")
-        player1_input = input("please select a square (1-9): ")
-        if player1_input == "1":
-            if board[0][0] == "X" or board[0][0] == "O":
-                print("square taken, pick again")
-            board[0][0] = "X"
-        elif player1_input == "2":
-            if board[0][1] == "X" or board[0][1] == "O":
-                print("square taken, pick again")
-            board[0][1] = "X"
-        elif player1_input == "3":
-            if board[0][2] == "X" or board[0][2] == "O":
-                print("square taken, pick again")
-            board[0][2] = "X"
-        elif player1_input == "4":
-            if board[1][0] == "X" or board[1][0] == "O":
-                print("square taken, pick again")
-            board[1][0] = "X"
-        elif player1_input == "5":
-            if board[1][1] == "X" or board[1][1] == "O":
-                print("square taken, pick again")  
-            board[1][1] = "X"
-        elif player1_input == "6":
-            if board[1][2] == "X" or board[1][2] == "O":
-                print("square taken, pick again")
-            board[1][2] = "X"
-        elif player1_input == "7":
-            if board[2][0] == "X" or board[2][0] == "O":
-                print("square taken, pick again")
-            board[2][0] = "X"
-        elif player1_input == "8":
-            if board[2][1] == "X" or board[2][1] == "O":
-                print("square taken, pick again")
-            board[2][1] = "X"
-        elif player1_input == "9":
-            if board[2][2] == "X" or board[2][2] == "O":
-                print("square taken, pick again")
-            board[2][2] = "X"
-        else:
-            print("invalid input")
-        print_board()
-
-
-player1turn()
+        while True:
+            print_board()
+            player1_input = input("please select a square (1-9): ")
+            if player1_input == "1":
+                if board[0][0] == "X" or board[0][0] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[0][0] = "X"
+                break
+            elif player1_input == "2":
+                if board[0][1] == "X" or board[0][1] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[0][1] = "X"
+                break
+            elif player1_input == "3":
+                if board[0][2] == "X" or board[0][2] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[0][2] = "X"
+                break
+            elif player1_input == "4":
+                if board[1][0] == "X" or board[1][0] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[1][0] = "X"
+                break
+            elif player1_input == "5":
+                if board[1][1] == "X" or board[1][1] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[1][1] = "X"
+                break
+            elif player1_input == "6":
+                if board[1][2] == "X" or board[1][2] == "O":
+                    continue
+                board[1][2] = "X"
+                break
+            elif player1_input == "7":
+                if board[2][0] == "X" or board[2][0] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[2][0] = "X"
+                break
+            elif player1_input == "8":
+                if board[2][1] == "X" or board[2][1] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[2][1] = "X"
+                break
+            elif player1_input == "9":
+                if board[2][2] == "X" or board[2][2] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[2][2] = "X"
+                break
+            else:
+                print("invalid input")
+                continue
+            break
+        
+def player2turn():  
+    if player2_turn == True: 
+        print("player 2 turn")
+        while True:
+            print_board()
+            player2_input = input("please select a square (1-9): ")
+            if player2_input == "1":
+                if board[0][0] == "X" or board[0][0] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[0][0] = "O"
+                break
+            elif player2_input == "2":
+                if board[0][1] == "X" or board[0][1] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[0][1] = "O"
+                break
+            elif player2_input == "3":
+                if board[0][2] == "X" or board[0][2] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[0][2] = "O"
+                break
+            elif player2_input == "4":
+                if board[1][0] == "X" or board[1][0] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[1][0] = "O"
+                break
+            elif player2_input == "5":
+                if board[1][1] == "X" or board[1][1] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[1][1] = "O"
+                break
+            elif player2_input == "6":
+                if board[1][2] == "X" or board[1][2] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[1][2] = "O"
+                break
+            elif player2_input == "7":
+                if board[2][0] == "X" or board[2][0] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[2][0] = "O"
+                break
+            elif player2_input == "8":
+                if board[2][1] == "X" or board[2][1] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[2][1] = "O"
+                break
+            elif player2_input == "9":
+                if board[2][2] == "X" or board[2][2] == "O":
+                    print("square taken, pick again")
+                    continue
+                board[2][2] = "O"
+                break
+            else:
+                print("invalid input")
+                continue
 
 player1turn()
